3bf3f3d85594806ab0f6dccd5750f975551ff591
alexwallden <alexander.wallden@me.com>
2023-02-07 13:56:55 +0100

    created tests for movie search

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -6,4 +6,13 @@ describe('first tests to try cypress', () => {
   it('should write in input', () => {
     cy.get('#searchText').type('HEJHEJ');
   });
-})
+});
+
+describe('test input functionality', () => {
+  it('should render movies with h3 elements', () => {
+    cy.get('#searchText').type('Die hard');
+    cy.get('#search').click();
+    cy.get('.movie:first-child > h3').should('contain.text', 'Die Hard');
+  });
+});
+
