729758fbc5858db303c2512ebb123015fe643227
alexwallden <alexander.wallden@me.com>
2023-02-08 15:21:33 +0100

    added tests and mock data for get request

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -14,5 +14,29 @@ describe('test input functionality', () => {
     cy.get('#search').click();
     cy.get('.movie:first-child > h3').should('contain.text', 'Die Hard');
   });
+
+  it('should render a man called otto', () => {
+    cy.intercept('GET', 'http://omdbapi.com/*', { fixture: "omdbResponse" }).as('omdbCall');
+
+
+    cy.get('#searchText').type('Die hard');
+    cy.get('#search').click();
+
+    cy.wait('@omdbCall').its('request.url').should('contain', 'Die%20hard');
+    cy.get('.movie').eq(0).get('h3').should('contain.text', 'A Man Called Otto');
+    cy.get('.movie').eq(1).get('h3').should('contain.text', 'Dune');
+  });
 });
+
+describe('test handling of response data', () => {
+  it('should display message for no results', () => {
+    cy.intercept('GET', 'http://omdbapi.com/*', {fixture: 'omdbEmptyResponse'}).as('omdbCall');
+
+    cy.get('#searchText').type('Ska returnera "Inga sökresultat att visa"');
+    cy.get('#search').click();
+
+    cy.get('p').should('contain.text', 'Inga sökresultat att visa');
+  })
+})
+
 
